655c4ef274f2546f34ecae9993300ee7fb9db8ec
Pratham Kukudkar <87323200+PRATHAMPRAJU@users.noreply.github.com>
2024-09-30 23:21:41 +0530

    Update main.tf

--- Security Group/main.tf
+++ Security Group/main.tf
@@ -27,18 +27,7 @@ resource "aws_security_group" "SG-01" {
 # security group for private
 resource "aws_security_group" "SG-02" {
   vpc_id      = var.aws_vpc
-  ingress  {
-    from_port   = 9090
-    to_port     = 9090
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress  {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  
   ingress  {
     from_port   = 3000
     to_port     = 3000
@@ -49,18 +38,6 @@ resource "aws_security_group" "SG-02" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress  {
-    from_port   = 9100
-    to_port     = 9100
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress  {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1" 
     cidr_blocks = ["0.0.0.0/0"]
   }
   
