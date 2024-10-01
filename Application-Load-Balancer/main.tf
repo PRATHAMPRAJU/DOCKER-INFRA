5c3863da65b85ca995a5c80253840ca8f13bd66f
Pratham Kukudkar <87323200+PRATHAMPRAJU@users.noreply.github.com>
2024-10-01 17:26:35 +0530

    Delete Application-Load-Balancer directory

--- Application-Load-Balancer/main.tf
+++
@@ -1,8 +0,0 @@
-# Application Load Balancer
-resource "aws_lb" "Docker-alb" {
-  name               = var.ALB-Name
-  internal           = false
-  load_balancer_type = var.ALB-type
-  security_groups    = [var.SG-02_id]
-  subnets            = [var.sub-pub-1-id , var.sub-pub-2-id]
-}
--- Application-Load-Balancer/output.tf
+++
@@ -1,7 +0,0 @@
-output "Docker-alb-arn" {
-  value = aws_lb.Docker-alb.arn
-}
-
-output "Docker-alb-id" {
-  value = aws_lb.Docker-alb.id
-}
