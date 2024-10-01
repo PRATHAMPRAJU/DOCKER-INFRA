84ff68bbd7fe24f3f07644796ea48bf32595d8e1
Pratham Kukudkar <87323200+PRATHAMPRAJU@users.noreply.github.com>
2024-10-01 17:27:21 +0530

    Delete IGW directory

--- IGW/output.tf
+++
@@ -1,3 +0,0 @@
-output "aws_internet_gateway_id" {
-  value = aws_internet_gateway.igw.id
-}
--- IGW/variable.tf
+++
@@ -1,8 +0,0 @@
-variable "aws_internet_gateway_tags" {
-  type = string
-  default = "pr-IGW"
-}
-
-variable "aws_vpc_id" {
-  type = string
-}
